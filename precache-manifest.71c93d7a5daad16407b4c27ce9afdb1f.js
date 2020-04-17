d16c46544361205f669e708f9e679a32bba6cb70
Yann Gouffon <github@yago-art.ch>
2020-04-17 17:19:24 +0200

    Updates

---
+++ precache-manifest.71c93d7a5daad16407b4c27ce9afdb1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dd94e828af264fb8267cc3952431523",
+    "url": "/react-pswp/index.html"
+  },
+  {
+    "revision": "52aa02bfce0285d9f24f",
+    "url": "/react-pswp/static/css/main.46455362.chunk.css"
+  },
+  {
+    "revision": "db4cd4386e8df3c8fb38",
+    "url": "/react-pswp/static/js/2.5cea080c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pswp/static/js/2.5cea080c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52aa02bfce0285d9f24f",
+    "url": "/react-pswp/static/js/main.1e783eb8.chunk.js"
+  },
+  {
+    "revision": "0c5b843b656b4c6f2f9421bbc818c3cf",
+    "url": "/react-pswp/static/js/main.1e783eb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25610d37bc5dc8a864f3",
+    "url": "/react-pswp/static/js/runtime-main.16259307.js"
+  }
+]);
